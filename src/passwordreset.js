c5410e657c5d4efe91f174929959c4d1def09bf1
Julio Gonzalez <julioegon@gmail.com>
2020-11-05 18:26:29 +0100

    Part3 running

--- src/passwordreset.js
+++ src/passwordreset.js
@@ -1,25 +1,3 @@
-// getCurrentDisplay() {
-//     const step = this.StaticRange.step;
-//     if (step=='something'){
-//         return(
-//             <div>show 1st display</div>
-//         )
-//     } else if(step=='somethingelse'){
-//         return(
-//             <div>show 2nd display</div>
-//         )
-//     } else {
-//         // and so on and so forth
-//     }
-// }
-
-
-// render()    {
-//     return(
-//         <div>{this.getCurrentDisplay()}</div>
-//     )
-// }
-
 import React from "react";
 import axios from "./axios";
 import { Link } from "react-router-dom";
